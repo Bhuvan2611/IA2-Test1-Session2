eb87f8fff850e217ba826b9f086c3d5c3f589cee
Bhuvan2611 <96898387+Bhuvan2611@users.noreply.github.com>
2022-02-21 09:55:28 +0000

    p7 done

--- p7final.c
+++ p7final.c
@@ -21,7 +21,7 @@ void find_area(Triangle *t)
 void output(Triangle t)
 {
   printf("The area of triangle with base = %f and altitude = %f is %f\n", t.base, t.altitude, t.area);
-}
+  }
 
 int main()
 {
